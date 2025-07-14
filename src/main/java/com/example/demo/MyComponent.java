71420bb274e25c0c737ca3a4b33a4dc2c5d1ca56
K4\Chung <cwchung0827@hotmail.com>
2025-07-15 00:09:39 +0800

    test

--- src/main/java/com/example/demo/MyComponent.java
+++ src/main/java/com/example/demo/MyComponent.java
@@ -4,15 +4,29 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.aia.oae.SingletonVariable;
 import com.example.demo.bean.Person;
 
 @Component
 public class MyComponent {
 	
-	
+	@Autowired
     private Person myService; // This should trigger the rule
+	
+	@Autowired
+    private Person myService2; // This should trigger the rule
     
+	@SingletonVariable
+	private String test2;
+	
     private String test;
+
+    private int test3;
+
+	@SingletonVariable	
+    private int test4;
+    
+    public static final String AAA = "AAA";
     
     public void doSomething() {
         
--- src/main/java/com/example/demo/bean/Person.java
+++ src/main/java/com/example/demo/bean/Person.java
@@ -1,7 +1,5 @@
 package com.example.demo.bean;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +8,12 @@ import com.example.annotation.SingletonVariable;
 @Component
 @Scope("singleton") // change to prototype
 public class Person {
-
+	
 	@SingletonVariable
 	private String name;
-
+	
 	public Person personSingleton() {
 		return new Person();
 	}
 
-	Boolean isSameNumberValue(AtomicLong a, AtomicLong b) {
-		return a.equals(b); // Noncompliant, this is true only if a == b
-	}
 }
