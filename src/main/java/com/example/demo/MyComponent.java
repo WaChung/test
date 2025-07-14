df3ec5174b1c5f9a440b40ce0ae214767885fc3d
K4\Chung <cwchung0827@hotmail.com>
2025-07-15 00:04:36 +0800

    test

--- src/main/java/com/example/demo/MyComponent.java
+++ src/main/java/com/example/demo/MyComponent.java
@@ -8,9 +8,12 @@ import com.example.demo.bean.Person;
 
 @Component
 public class MyComponent {
-    @Autowired
+	
+	
     private Person myService; // This should trigger the rule
-
+    
+    private String test;
+    
     public void doSomething() {
         
     }
