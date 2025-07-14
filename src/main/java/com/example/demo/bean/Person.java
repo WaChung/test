90d6aa92c62d05cba7e70893203e190722434ed9
K4\Chung <cwchung0827@hotmail.com>
2025-07-14 15:10:23 +0800

    test

--- src/main/java/com/example/demo/bean/Person.java
+++ src/main/java/com/example/demo/bean/Person.java
@@ -1,5 +1,7 @@
 package com.example.demo.bean;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +10,15 @@ import com.example.annotation.SingletonVariable;
 @Component
 @Scope("singleton") // change to prototype
 public class Person {
-	
+
 	@SingletonVariable
 	private String name;
-	
+
 	public Person personSingleton() {
 		return new Person();
 	}
 
+	Boolean isSameNumberValue(AtomicLong a, AtomicLong b) {
+		return a.equals(b); // Noncompliant, this is true only if a == b
+	}
 }
